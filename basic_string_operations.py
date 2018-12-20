2ba1b3d2dd42dd0b604c6253b83f1bbaf5535f63
Aleksei Ivhsin <aleksei_ivshin@epam.com>
2018-12-20 14:16:20 +0400

    Basic string operations

---
+++ basic_string_operations.py
@@ -0,0 +1,54 @@
+print("Practice")
+singleQuotes = 'Single quotes string'
+doubleQuotes = "Double quotes stirng"
+
+astring = "Hello_world"
+print(astring.index("o"))
+print(astring.count("l"))
+print(astring[3:])
+print(astring[3:7])
+print("1234567890"[1:7:2])
+print(astring[-3:])
+print(astring[::-1])
+print(astring.lower())
+print(astring.upper())
+print(astring.startswith("Hell"))
+print(astring.endswith("lol"))
+print(astring.split("_"))
+
+print("Exercice")
+
+s = "There are a least 20"
+# Length should be 20
+print("Length of s = %d" % len(s))
+
+# First occurrence of "a" should be at index 8
+print("The first occurrence of the letter a = %d" % s.index("a"))
+
+# Number of a's should be 2
+print("a occurs %d times" % s.count("a"))
+
+# Slicing the string into bits
+print("The first five characters are '%s'" % s[:5]) # Start to 5
+print("The next five characters are '%s'" % s[5:10]) # 5 to 10
+print("The thirteenth character is '%s'" % s[12]) # Just number 12
+print("The characters with odd index are '%s'" %s[1::2]) #(0-based indexing)
+print("The last five characters are '%s'" % s[-5:]) # 5th-from-last to end
+
+# Convert everything to uppercase
+print("String in uppercase: %s" % s.upper())
+
+# Convert everything to lowercase
+print("String in lowercase: %s" % s.lower())
+
+# Check how a string starts
+if s.startswith("Str"):
+    print("String starts with 'Str'. Good!")
+
+# Check how a string ends
+if s.endswith("ome!"):
+    print("String ends with 'ome!'. Good!")
+
+# Split the string into three separate strings,
+# each containing only a word
+print("Split the words of the string: %s" % s.split(" "))
