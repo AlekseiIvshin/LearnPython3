1e93ef36774aa17ffb3fd6c268ef85d5f4b6e637
Aleksei Ivhsin <aleksei_ivshin@epam.com>
2019-03-19 11:13:46 +0400

    Added singly linked list

--- Banana/neural.py
+++ Banana/neural.py
@@ -28,12 +28,3 @@ class NeuralNetwork:
     self.weights1 += d_weights1
     self.weights2 += d_weights2
 
-if __name__ == "__main__":
-  X = np.array([[0,0,1],[0,1,1],[1,0,1],[1,1,1]])
-  y = np.array([[0],[1],[1],[0]])
-  nn = NeuralNetwork(X,y)
-
-  for i in range(1500):
-    nn.feedforward()
-    nn.backprop()
-  print(nn.output)
---
+++ Banana/trainer.py
@@ -0,0 +1,19 @@
+import numpy as np
+from neural import NeuralNetwork
+
+def sample():
+    X = np.array([[0,0,1],
+                  [0,1,1],
+                  [1,0,1],
+                  [1,1,1]])
+    y = np.array([[0],[1],[1],[0]])
+    nn = NeuralNetwork(X,y)
+
+    for i in range(1500):
+        nn.feedforward()
+        nn.backprop()
+
+    print(nn.output)
+
+if __name__ == "__main__":
+    sample()
